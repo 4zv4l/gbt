7d7360ab0c3027266c64c499405128badbd30121
4zv4l <4zv4l@protonmail.com>
2026-03-18 16:30:20 +0800

    feat: handle udp tracker (basic)

--- bittorrent/tracker.go
+++ bittorrent/tracker.go
@@ -4,8 +4,12 @@ import (
 	"encoding/binary"
 	"fmt"
 	"io"
+	"math/rand"
+	"net"
 	"net/http"
 	"net/netip"
+	"net/url"
+	"strconv"
 	"strings"
 	"time"
 
@@ -31,6 +35,18 @@ func rawPeersToAddrPort(rawPeers []byte) []netip.AddrPort {
 
 // Retrieving peers from the tracker
 func GetPeersFromTracker(trackerUrl string) (TrackerReply, error) {
+	proto := strings.Split(trackerUrl, ":")[0]
+	switch proto {
+	case "http":
+		return httpTracker(trackerUrl)
+	case "udp":
+		return udpTracker(trackerUrl)
+	default:
+		return TrackerReply{}, fmt.Errorf("%s tracker not supported", proto)
+	}
+}
+
+func httpTracker(trackerUrl string) (TrackerReply, error) {
 	resp, err := http.Get(trackerUrl)
 	if err != nil {
 		return TrackerReply{}, err
@@ -55,6 +71,54 @@ func GetPeersFromTracker(trackerUrl string) (TrackerReply, error) {
 	return TrackerReply{
 		Interval: reply["interval"].(int),
 		Peers:    rawPeersToAddrPort([]byte(reply["peers"].(string))),
+	}, nil
+}
+
+func udpTracker(trackerUrl string) (TrackerReply, error) {
+	u, _ := url.Parse(trackerUrl)
+	q := u.Query()
+	port, _ := strconv.Atoi(q.Get("port"))
+	left, _ := strconv.ParseUint(q.Get("left"), 10, 64)
+
+	conn, err := net.DialTimeout("udp", u.Host, 5*time.Second)
+	if err != nil {
+		return TrackerReply{}, err
+	}
+	defer conn.Close()
+	conn.SetDeadline(time.Now().Add(5 * time.Second))
+
+	req := make([]byte, 98)
+	resp := make([]byte, 2048)
+	txID := rand.Uint32()
+
+	// "handshake" (tracker will reply with an ID)
+	binary.BigEndian.PutUint64(req[0:8], 0x41727101980) // Magic Number
+	binary.BigEndian.PutUint32(req[12:16], txID)        // Action is already 0 by default
+
+	conn.Write(req[:16])
+	if _, err := conn.Read(resp); err != nil || binary.BigEndian.Uint32(resp[4:8]) != txID {
+		return TrackerReply{}, fmt.Errorf("udp connect failed or timeout")
+	}
+
+	// announce
+	binary.BigEndian.PutUint64(req[0:8], binary.BigEndian.Uint64(resp[8:16])) // Copy Connection ID from response
+	binary.BigEndian.PutUint32(req[8:12], 1)                                  // Action: 1 (Announce)
+	copy(req[16:36], q.Get("info_hash"))
+	copy(req[36:56], q.Get("peer_id"))
+	binary.BigEndian.PutUint64(req[64:72], left)
+	binary.BigEndian.PutUint32(req[92:96], 0xFFFFFFFF) // NumWant: -1 (tracker send default amount of peers)
+	binary.BigEndian.PutUint16(req[96:98], uint16(port))
+
+	conn.Write(req)
+	n, err := conn.Read(resp)
+	if err != nil || n < 20 || binary.BigEndian.Uint32(resp[0:4]) != 1 {
+		return TrackerReply{}, fmt.Errorf("udp announce failed")
+	}
+
+	fmt.Println(rawPeersToAddrPort(resp[20:n]))
+	return TrackerReply{
+		Interval: int(binary.BigEndian.Uint32(resp[8:12])),
+		Peers:    rawPeersToAddrPort(resp[20:n]),
 	}, nil
 }
 
